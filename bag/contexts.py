0692c9df280efd6639b7894ac8e67f3563b58aa8
VictoriaT87 <victoriaemt@gmail.com>
2023-04-19 11:28:18 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,11 +9,32 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    # get bag session
+    bag = request.session.get('bag', {})
 
+    # for each idem id and the quantity in bag session
+    for item_id, quantity in bag.items():
+        #get the product
+        product = get_object_or_404(Product, pk=item_id)
+        # total is quantity times the price
+        total += quantity * product.price
+        # incremenet product count is quantity
+        product_count += quantity
+        # append the bag with each context
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    # id the total price is less than the free delivery price in settings
     if total < settings.FREE_DELIVERY_THRESHOLD:
+        # delivery = total price times decimal percentage of delivery price
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        # delivery is total minus the threshold price
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
+        # else delivery is free
         delivery = 0
         free_delivery_delta = 0
     
--- bag/views.py
+++ bag/views.py
@@ -12,15 +12,18 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    # quantity is int number of string number from template
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    # bag session, ends when window closes
     bag = request.session.get('bag', {})
 
+    # if item id is in bag session, add to quantity
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+    # else quantity stays the same
     else:
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
