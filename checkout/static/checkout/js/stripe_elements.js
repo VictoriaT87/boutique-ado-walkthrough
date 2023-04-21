0d198f69e23b7c7d74a0b949ecb6c94667ad2410
VictoriaT87 <victoriaemt@gmail.com>
2023-04-21 14:13:47 +0000

    add stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,33 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+// get key in text for, slice off quotation at beginning and end
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+// stripe variable from stripe script in base.html
+var stripe = Stripe(stripe_public_key);
+// create elements for card payment
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+// create card element
+var card = elements.create('card', {style: style});
+// mount card elements to element div on template
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MzJbxLfBKy5nHPe0V6eLgk15XLRp8ScT7hr91fjZKMHoHxGbkbgjRjFCfpZbIJvin98WM6RNAke54XRJwBYGBCH00R6zBZHmE',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
