f9cf6c65774269bac46dcba32760392f29daca42
VictoriaT87 <victoriaemt@gmail.com>
2023-04-22 10:47:16 +0000

    Update checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,24 +20,24 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         # cursor starts on Full Name page on load
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                # put star on fields required
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            # placeholder names from dictionary above
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    # put star on fields required
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                # placeholder names from dictionary above
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # add css class used later
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             # remove labels because we have placeholders
