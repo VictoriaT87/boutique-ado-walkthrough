9ac9dac637d3f53a7de46949103c0da12986b13c
VictoriaT87 <victoriaemt@gmail.com>
2023-04-22 10:38:52 +0000

    Complete webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-import uuid # generate order number
+import uuid  # generate order number
 
 from django.db import models
 from django.db.models import Sum
@@ -33,6 +33,9 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         # underscore means private method, only used inside class
@@ -79,7 +82,7 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-        # not editable, automatically calculated
+    # not editable, automatically calculated
 
     def save(self, *args, **kwargs):
         """
